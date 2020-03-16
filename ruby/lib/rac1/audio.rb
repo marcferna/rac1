8d75c8b454ca42d303b46f13397fcf5107742e3c
Marc Fernandez <marc.fernandezg@gmail.com>
2020-03-15 18:13:38 -0700

    Working version with dates.

--- ruby/lib/rac1/audio.rb
+++ ruby/lib/rac1/audio.rb
@@ -1,11 +1,15 @@
 require 'faraday'
-require 'pry'
+require 'json'
 
 class Rac1::Audio
-  def piece(audio_id:)
+  def initialize(audio_id:)
+    @audio_id = audio_id
+  end
+
+  def url
     response = Faraday.get(
       "https://api.audioteca.rac1.cat/piece/audio",
-      { id: audio_id }
+      { id: @audio_id }
     )
 
     JSON.parse(response.body)['path']
--- ruby/lib/rac1/search.rb
+++ ruby/lib/rac1/search.rb
@@ -4,22 +4,23 @@ require 'nokogiri'
 
 module Rac1
   class Search
-    URL = 'https://api.audioteca.rac1.cat'
+    URL = 'https://api.audioteca.rac1.cat'.freeze
 
-    def search(date: "06/03/2020")
+    def search(date: Time.new)
       response = Faraday.get(
         "#{URL}/a-la-carta/cerca",
         {
           programId: program,
           sectionId: section,
-          from:      date,
-          to:        "07/03/2020"
+          from:      date.strftime('%d/%m/%Y'),
+          to:        (date + 60 * 60 * 24).strftime('%d/%m/%Y')
         }
       )
 
       audio_id = Nokogiri::HTML(response.body).xpath(
         '//div[@class="audioteca-listed-search"]//ul//li'
-      ).first['data-audio-id']
+      )&.first&.[]('data-audio-id')
+
       return audio_id
     end
   end
